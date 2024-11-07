a38d0fcfbdb9218a34ba88afe3c47d59c3707b88
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2024-11-07 09:05:13 +0000

    Added the initial code

---
+++ test_zoo.py
@@ -0,0 +1,14 @@
+import unittest
+from zoo import Zoo
+
+class TestZoo(unittest.TestCase):
+    def setUp(self):
+        self.zoo = Zoo()
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+       
+    # Add your additional test cases here.
+
+if __name__ == '__main__':
+    unittest.main()
